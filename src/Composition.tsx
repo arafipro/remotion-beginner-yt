cc841670abf2d179e50b78ac5727be2381b4ddbc
Hiroki Chiyoda <contact@arafipro.com>
2023-10-22 11:50:10 +0900

    first commit

--- src/Composition.tsx
+++ src/Composition.tsx
@@ -1,3 +1,16 @@
+import { AbsoluteFill, useCurrentFrame } from "remotion";
 export const MyComposition = () => {
-	return null;
+  const frame = useCurrentFrame();
+  return (
+    <AbsoluteFill
+      style={{
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: 100,
+        backgroundColor: "white",
+      }}
+    >
+      The current frame is {frame}.
+    </AbsoluteFill>
+  );
 };
